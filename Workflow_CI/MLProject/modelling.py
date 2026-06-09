3dc349f30ec6fad0b25334d481c2b5234edd32a0
student-01-bcd9b7e9b263 <echostudent59@gmail.com>
2026-06-09 19:45:45 +0700

    Update modelling.py

--- Workflow_CI/MLProject/modelling.py
+++ Workflow_CI/MLProject/modelling.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import dagshub
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-
-# Inisialisasi DagsHub (Pastikan username dan repo sesuai)
-dagshub.init(repo_owner='sinagabosar16', repo_name='Eksperimen_SML_Bosar-Martua-Sinaga', mlflow=True)
+import os
+import shutil
 
 def train_model():
-    # Karena dijalankan via MLproject, path data cukup nama filenya saja 
-    # asalkan berada di direktori yang sama (MLProject folder)
+    # Path disesuaikan karena dijalankan di dalam folder MLProject
     data_path = "ispu_dki1_preprocessing.csv"
     print(f"Membaca dataset: {data_path}")
     df = pd.read_csv(data_path)
@@ -22,7 +19,11 @@ def train_model():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # MLflow Tracking
+    # Memastikan folder model_output bersih sebelum diisi model baru
+    if os.path.exists("model_output"):
+        shutil.rmtree("model_output")
+
+    # MLflow Tracking (Lokal)
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
@@ -34,9 +35,11 @@ def train_model():
 
         mlflow.log_param("model_type", "RandomForest")
         mlflow.log_metric("accuracy", acc)
-        mlflow.sklearn.log_model(model, "model")
         
-        print("Model berhasil ditraining dan dicatat di MLflow!")
+        # MENYIMPAN MODEL LOKAL UNTUK DOCKER (SYARAT ADVANCE)
+        mlflow.sklearn.save_model(model, "model_output")
+        
+        print("Model berhasil ditraining dan disimpan ke folder 'model_output'!")
 
 if __name__ == "__main__":
     train_model()
